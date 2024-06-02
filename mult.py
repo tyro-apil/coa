1ab06d2c0841276307a5c54f872d5ba0bba31ff8
apil <078bct017.apil@pcampus.edu.np>
2024-06-02 15:20:28 +0545

    add multiply example

---
+++ mult.py
@@ -0,0 +1,10 @@
+from utils import Multiply
+
+def main():
+  a=input("Enter first number: ")
+  b=input("Enter second number: ")
+  mult = Multiply(a,b)
+  print(mult)
+
+if __name__=="__main__":
+  main()
