ab830da10bdeef11b4bd68e4973be227d9b67cfa
apil <078bct017.apil@pcampus.edu.np>
2024-06-02 13:14:15 +0545

    fix: full_adder and add: full_subtractor

--- utils.py
+++ utils.py
@@ -27,7 +27,45 @@ def FullAdder(num1, num2):
   """
   sum = ''
   carry = '0'
+  num1, num2 = add_zero_padding(num1, num2)
+  num1, num2 = num1[::-1], num2[::-1]
   for bit1, bit2 in zip(num1, num2):
     sum += EX_OR(EX_OR(bit1, bit2),carry)
     carry = OR(AND(bit1, bit2), AND(carry, EX_OR(bit1, bit2)))
-  return sum, carry
+  return sum[::-1], carry[::-1]
+
+def add_zero_padding(num1: str, num2: str):
+  max_len = max(len(num1), len(num2))
+
+  num1 = num1.zfill(max_len)
+  num2 = num2.zfill(max_len)
+
+  return num1, num2
+
+def ONES_COMP(a:str):
+  ones_comp = ""
+  for char in a:
+    if char=='0':
+      ones_comp += '1'
+    else:
+      ones_comp += '0'
+  return ones_comp
+
+def TWOS_COMP(a:str):
+  ones_comp = ONES_COMP(a)
+  sum, carry = FullAdder(ones_comp, '1')
+  if carry != '0':
+    return carry + sum
+  return sum
+
+def FullSubtractor(a: str, b: str):
+  a,b = add_zero_padding(a,b)
+  twos_comp_b = TWOS_COMP(b)
+  sum, carry = FullAdder(a, twos_comp_b)
+  borrow = ONES_COMP(carry)
+  if borrow == '0':
+    return '+'+sum
+  return '-'+TWOS_COMP(sum)
+
+
+  
